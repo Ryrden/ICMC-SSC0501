5acb7aaea227114333e0cec177d565fd0ff18f84
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-07-05 11:46:56 -0300

    Create Algoritmo da divisão.c

---
+++ Códigos feitos por Livre e espontânea vontade/Algoritmo da divisão.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void caso1 (int a,int b, int *q, int *r){
+
+    *q = b / a;
+
+    *r = b % a;
+
+}
+
+
+int main(){
+    setlocale(LC_ALL, "");
+
+    int dividendo,divisor,quociente, resto;
+
+    printf("Considerando o Algoritmo como b|a, tal que a = bq + r \nDigite b: ");
+    scanf("%d", &divisor);
+
+    printf("\nDigite a: ");
+    scanf("%d", &dividendo);
+
+
+    if (dividendo > 0 && divisor > 0){
+
+        caso1(dividendo, divisor, &quociente, &resto);
+
+        printf("a = %d \nb = %d \nq = %d \nr = %d", dividendo,divisor,quociente, resto);
+    }
+
+    printf("\n\n");
+    system("pause");
+ return;
+}
