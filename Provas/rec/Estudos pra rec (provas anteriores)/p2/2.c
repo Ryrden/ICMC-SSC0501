aad4610cde842b1a1b221041028a216d39b1612b
Ryan Souza <76923948+Ryrden@users.noreply.github.com>
2021-08-20 00:36:04 -0300

    Update 2.c

--- Provas/rec/Estudos pra rec (provas anteriores)/p2/2.c
+++ Provas/rec/Estudos pra rec (provas anteriores)/p2/2.c
@@ -24,16 +24,16 @@ int main() {
     char S[50], A[50], B[50];
     int contA = 0, contB = 0;
 
-    /* FUNCIONA!
-        scanf("%[^\n]s", S);
-        setbuf(stdin, NULL);
-        scanf("%s", A);
-        setbuf(stdin, NULL);
-        scanf("%s", B);
-        setbuf(stdin, NULL);
+/* FUNCIONA!
+    scanf("%[^\n]s", S);
+    setbuf(stdin, NULL);
+    scanf("%s", A);
+    setbuf(stdin, NULL);
+    scanf("%s", B);
+    setbuf(stdin, NULL);
 */
 
-    /* FUNCIONA Parcialmente (tem problemas [desconhecido])
+/* FUNCIONA Parcialmente (tem problemas [desconhecido])
     fgets(S, 50, stdin);
     S[strlen(S) - 1] = '\0';
     scanf("%s", A);
@@ -42,37 +42,37 @@ int main() {
     B[strlen(B) - 1] = '\0';
 */
 
-    /* FUNCIONA! (porém não é indicado ler strings somente com scanf [segurança])
-        scanf("%[^\n]s", S);
-        scanf("%s", A);
-        scanf("%s", B);
+/* FUNCIONA! (porém não é indicado ler strings somente com scanf [segurança])
+    scanf("%[^\n]s", S);
+    scanf("%s", A);
+    scanf("%s", B);
 */
 
-    /* NAO FUNCIONA!
-        scanf("%s", S); //Se o scanf não for da forma: scanf("%[^\n]s", S); a leitura para no primeiro caractere especial ' '
-        scanf("%s", A);
-        scanf("%s", B);
+/* NAO FUNCIONA!
+    scanf("%s", S); //Se o scanf não for da forma: scanf("%[^\n]s", S); a leitura para no primeiro caractere especial ' '
+    scanf("%s", A);
+    scanf("%s", B);
 */
 
-    /* NAO FUNCIONA! (No RunCodes)
-        fgets(S, 50, stdin);
-        S[strlen(S) - 1] = '\0';
-        setbuf(stdin, NULL);
-        fgets(A, 50, stdin);
-        A[strlen(A) - 1] = '\0';
-        setbuf(stdin, NULL);
-        fgets(B, 50, stdin);
-        B[strlen(B) - 1] = '\0';
-        setbuf(stdin, NULL);
+/* NAO FUNCIONA! (No RunCodes)
+    fgets(S, 50, stdin);
+    S[strlen(S) - 1] = '\0';
+    setbuf(stdin, NULL);
+    fgets(A, 50, stdin);
+    A[strlen(A) - 1] = '\0';
+    setbuf(stdin, NULL);
+    fgets(B, 50, stdin);
+    B[strlen(B) - 1] = '\0';
+    setbuf(stdin, NULL);
 */
 
-    /* NAO FUNCIONA (No RunCodes)
-        fgets(S, 50, stdin);
-        S[strlen(S) - 1] = '\0';
-        fgets(A, 50, stdin);
-        A[strlen(A) - 1] = '\0';
-        fgets(B, 50, stdin);
-        B[strlen(B) - 1] = '\0';
+/* NAO FUNCIONA (No RunCodes)
+    fgets(S, 50, stdin);
+    S[strlen(S) - 1] = '\0';
+    fgets(A, 50, stdin);
+    A[strlen(A) - 1] = '\0';
+    fgets(B, 50, stdin);
+    B[strlen(B) - 1] = '\0';
 */
     fgets(S, 50, stdin);
     S[strlen(S) - 1] = '\0';
